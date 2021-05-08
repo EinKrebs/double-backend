34ab607f9790e641187733c6b91b43cca4f9af5a
EinKrebs <yura200253@gmail.com>
2021-05-08 15:42:17 +0500

    Shitty shit

---
+++ double_backend/models.py
@@ -0,0 +1,113 @@
+from django.db import models
+from django.contrib import admin
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255,
+                            unique=True,)
+    icon = models.URLField(blank=True,
+                           # upload_to='images/category_icons',
+                           )
+
+    def __str__(self):
+        return f'Category {self.name}'
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'icon': self.icon if self.icon is not None else '',
+        }
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+class Level(models.Model):
+    name = models.CharField(max_length=255,
+                            unique=True)
+    code = models.CharField(max_length=2,
+                            unique=True)
+
+    def __str__(self):
+        return f'Level {self.name}: {self.code}'
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'code': self.code,
+        }
+
+
+@admin.register(Level)
+class LevelAdmin(admin.ModelAdmin):
+    pass
+
+
+class Theme(models.Model):
+    category = models.ForeignKey(Category,
+                                 on_delete=models.CASCADE)
+    level = models.ForeignKey(Level,
+                              on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    photo = models.URLField(blank=True,
+                            # upload_to='images/theme_photos',
+                            )
+
+    def __str__(self):
+        return f'Theme {self.name}'
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'category': self.category.id,
+            'level': self.level.id,
+            'name': self.name,
+            'photo': self.photo
+        }
+
+
+@admin.register(Theme)
+class ThemeAdmin(admin.ModelAdmin):
+    pass
+
+
+class Word(models.Model):
+    name = models.CharField(max_length=255)
+    translation = models.CharField(max_length=255)
+    transcription = models.CharField(max_length=255,
+                                     blank=True,
+                                     null=False)
+    example = models.TextField(blank=True,
+                               null=False)
+    sound = models.URLField(max_length=255,
+                            blank=True,
+                            null=False)
+    themes = models.ManyToManyField(Theme)
+
+    def __str__(self):
+        return f'Word {self.name}'
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'translation': self.translation,
+            'transcription': self.transcription,
+            'example': self.example,
+            'sound': self.sound,
+        }
+
+    def to_json_short(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+        }
+
+
+@admin.register(Word)
+class WordAdmin(admin.ModelAdmin):
+    pass
---
+++ double_backend/views.py
@@ -0,0 +1,43 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import JsonResponse
+from double_backend.models import (
+    Category,
+    Level,
+    Theme,
+    Word,
+)
+
+
+def get_all(cls):
+    def res(request):
+        objects = cls.objects.all()
+        jsons = list(map(cls.to_json, objects))
+        return JsonResponse(jsons, safe=False)
+
+    return res
+
+
+def get_id(request):
+    path = request.path
+    return path[path.find('/')]
+
+
+def get_theme(request, theme_id=None):
+    theme_id = int(theme_id)
+    try:
+        theme = Theme.objects.get(pk=theme_id)
+    except ObjectDoesNotExist:
+        return JsonResponse({})
+    words = Word.objects.filter(themes__pk=theme_id)
+    res = theme.to_json()
+    res['words'] = list(map(Word.to_json_short, words))
+    return JsonResponse(res)
+
+
+def get_word(request, word_id=None):
+    word_id = int(word_id)
+    try:
+        print(Word.objects.get(id=word_id).to_json())
+        return JsonResponse(Word.objects.get(id=word_id).to_json())
+    except ObjectDoesNotExist:
+        return JsonResponse({})
